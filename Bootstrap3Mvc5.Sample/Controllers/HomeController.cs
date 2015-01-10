bce32e4c1b0fb4c96641c693f53784452bec0585
Dmitry Popov <dmitry@parsec-club.ru>
2015-01-11 00:35:21 +0300

    + samples

---
+++ Bootstrap3Mvc5.Sample/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap3Mvc5.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Basic()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bootstrap3Mvc5.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bootstrap3Mvc5.Sample.MvcApplication" Language="C#" %>
