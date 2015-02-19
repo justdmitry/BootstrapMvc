26fc20a8a6f1ec38e470748c6b645b12bbc2ac8d
Dmitry Popov <dmitry@parsec-club.ru>
2015-02-19 13:12:33 +0300

    + samples website (in progress)

--- Bootstrap3Mvc5.Sample/App_Start/RouteConfig.cs
+++ Bootstrap3Mvc5.Sample/App_Start/RouteConfig.cs
@@ -15,9 +15,8 @@ namespace Bootstrap3Mvc5.Sample
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                url: "{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
--- Bootstrap3Mvc5.Sample/Controllers/HomeController.cs
+++ Bootstrap3Mvc5.Sample/Controllers/HomeController.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Web;
+﻿extern alias bootstrap3mvc5;
+
+using System.Collections.Specialized;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Bootstrap3Mvc5.Sample.Controllers
 {
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 60 * 60 * 24, Location = OutputCacheLocation.Any)]
         public ActionResult Index()
+        {
+            var versions = new NameValueCollection();
+            versions.Add("Bootstrapmvc.Core", typeof(BootstrapMvc.Core.WritableBlock).Assembly.GetName().Version.ToString());
+            versions.Add("Bootstrapmvc.Mvc5", typeof(BootstrapMvc.Core.BootstrapContext).Assembly.GetName().Version.ToString());
+            versions.Add("Bootstrapmvc.Bootstrap3", typeof(BootstrapMvc.Elements.Icon).Assembly.GetName().Version.ToString());
+            versions.Add("Bootstrapmvc.Bootstrap3Mvc5", typeof(bootstrap3mvc5::BootstrapMvc.AnyContentExtensions).Assembly.GetName().Version.ToString());
+            return View(versions);
+        }
+
+        public ActionResult Installation()
         {
             return View();
         }
 
-        public ActionResult Basic()
+        public ActionResult Basics()
+        {
+            return View();
+        }
+
+        public ActionResult Components()
+        {
+            return View();
+        }
+
+        public ActionResult Extend()
         {
             return View();
         }
