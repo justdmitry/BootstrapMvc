8ba93a0499b1404cd31bdc3d18789fc376149fc5
Dmitry Popov <dmitry@parsec-club.ru>
2015-10-21 11:54:52 +0300

    some extensions fixed

---
+++ src/BootstrapMvc.Core/IAnyContentMarkerExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace BootstrapMvc
+{
+    using System;
+    using BootstrapMvc.Core;
+
+    public static class IAnyContentMarkerExtensions
+    {
+        public static IItemWriter<T> CreateWriter<T>(this IAnyContentMarker target)
+            where T : IWritableItem, new()
+        {
+            return target.Context.Helper.CreateWriter<T>(target.Context.GetCurrentParent());
+        }
+
+        public static IItemWriter<T, TContent> CreateWriter<T, TContent>(this IAnyContentMarker target)
+            where T : ContentElement<TContent>, IWritableItem, new()
+            where TContent : DisposableContent
+        {
+            return target.Context.Helper.CreateWriter<T, TContent>(target.Context.GetCurrentParent());
+        }
+    }
+}
--- src/BootstrapMvc.Core/IControlContextExtensions.cs
+++ src/BootstrapMvc.Core/IControlContextExtensions.cs
@@ -5,17 +5,37 @@
 
     public static class IControlContextExtensions
     {
-        public static IItemWriter<T> CreateWriter<T>(this IAnyContentMarker target)
-            where T : IWritableItem, new()
+        public static TWriter FieldName<TWriter, TItem>(this TWriter target, string name)
+            where TWriter : IItemWriter<TItem>
+            where TItem : IControlContext
         {
-            return target.Context.Helper.CreateWriter<T>(target.Context.GetCurrentParent());
+            target.Item.FieldName = name;
+            return target;
         }
 
-        public static IItemWriter<T, TContent> CreateWriter<T, TContent>(this IAnyContentMarker target)
-            where T : ContentElement<TContent>, IWritableItem, new()
-            where TContent : DisposableContent
+        public static TWriter FieldValue<TWriter, TItem>(this TWriter target, object value)
+            where TWriter : IItemWriter<TItem>
+            where TItem : IControlContext
         {
-            return target.Context.Helper.CreateWriter<T, TContent>(target.Context.GetCurrentParent());
+            target.Item.FieldValue = value;
+            return target;
+        }
+
+        public static TWriter Required<TWriter, TItem>(this TWriter target, bool required = true)
+            where TWriter : IItemWriter<TItem>
+            where TItem : IControlContext
+        {
+            target.Item.IsRequired = required;
+            return target;
+        }
+        
+        public static TWriter Errors<TWriter, TItem>(this TWriter target, string[] errors)
+            where TWriter : IItemWriter<TItem>
+            where TItem : IControlContext
+        {
+            target.Item.Errors = errors;
+            target.Item.HasErrors = (errors != null && errors.Length > 0);
+            return target;
         }
     }
 }
