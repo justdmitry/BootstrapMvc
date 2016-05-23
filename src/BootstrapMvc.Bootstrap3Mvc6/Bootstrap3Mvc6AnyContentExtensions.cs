b961bd68531ff30b4e45920062fa49812567dc17
Dmitry Popov <dmitry@parsec-club.ru>
2016-05-23 13:29:51 +0300

    BootstrapMvc.Bootstrap3Mvc6 v2.1.0

--- src/BootstrapMvc.Bootstrap3Mvc6/Bootstrap3Mvc6AnyContentExtensions.cs
+++ src/BootstrapMvc.Bootstrap3Mvc6/Bootstrap3Mvc6AnyContentExtensions.cs
@@ -3,7 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using Microsoft.AspNet.Mvc.Rendering;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+
     using BootstrapMvc;
     using BootstrapMvc.Controls;
     using BootstrapMvc.Core;
