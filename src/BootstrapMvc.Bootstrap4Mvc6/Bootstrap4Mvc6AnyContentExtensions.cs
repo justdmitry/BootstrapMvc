7b6555726afddbadd6b3e92837c4ef36d09b7afc
Dmitry Popov <dmitry@parsec-club.ru>
2016-08-18 18:28:07 +0300

    Bootstrap4Mvc6: AnyContentExtensions is back

--- src/BootstrapMvc.Bootstrap4Mvc6/Bootstrap4Mvc6AnyContentExtensions.cs
+++ src/BootstrapMvc.Bootstrap4Mvc6/Bootstrap4Mvc6AnyContentExtensions.cs
@@ -6,21 +6,21 @@
     using Microsoft.AspNetCore.Mvc.Rendering;
 
     using BootstrapMvc;
-    //using BootstrapMvc.Controls;
+    using BootstrapMvc.Controls;
     using BootstrapMvc.Core;
 
     public static class Bootstrap4Mvc6AnyContentExtensions
     {
-        //public static IItemWriter<Select, SelectContent> Select(this IAnyContentMarker contentHelper, IEnumerable<SelectListItem> items)
-        //{
-        //    var select = contentHelper.Select();
-        //    select.Item.Items = items.Select(x => SelectListItemToSelectOption(contentHelper.Context, x));
-        //    return select;
-        //}
+        public static IItemWriter<Select, SelectContent> Select(this IAnyContentMarker contentHelper, IEnumerable<SelectListItem> items)
+        {
+            var select = contentHelper.Select();
+            select.Item.Items = items.Select(x => SelectListItemToSelectOption(contentHelper.Context, x));
+            return select;
+        }
 
-        //private static SelectOption SelectListItemToSelectOption(IBootstrapContext context, SelectListItem item)
-        //{
-        //    return context.Helper.CreateWriter<SelectOption, AnyContent>(null).Value(item.Value).Disabled(item.Disabled).Content(item.Text).Item;
-        //}
+        private static SelectOption SelectListItemToSelectOption(IBootstrapContext context, SelectListItem item)
+        {
+            return context.Helper.CreateWriter<SelectOption, AnyContent>(null).Value(item.Value).Disabled(item.Disabled).Content(item.Text).Item;
+        }
     }
 }
