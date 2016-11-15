74a9dc3eeba7c5f33e868fce9a43f0fa294c9f08
Dmitry Popov <dmitry@parsec-club.ru>
2016-11-16 00:55:49 +0300

    Samples updated

--- samples/Bootstrap4Mvc6.Sample/Models/DemoModelOne.cs
+++ samples/Bootstrap4Mvc6.Sample/Models/DemoModelOne.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bootstrap4Mvc6.Sample.Models
 {
     public class DemoModelOne
     {
+        [Display(Name = "Some string")]
         public string StringField { get; set; }
 
+        [Display(Name = "Some bool")]
         public bool BooleanField { get; set; }
 
+        [Display(Name = "Some int")]
         public int IntegerField { get; set; }
 
         public DateTimeOffset DateTimeOffsetField { get; set; } = DateTimeOffset.Now;
