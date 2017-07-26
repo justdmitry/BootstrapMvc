8067f8eae6add550619fd31349ed3d3d9d648a91
Dmitry Popov <dmitry@parsec-club.ru>
2017-07-26 17:09:15 +0300

    Possible solution for Issue #9

---
+++ src/BootstrapMvc.BootstrapCommon/Components/Forms/HiddenValue.cs
@@ -0,0 +1,49 @@
+﻿namespace BootstrapMvc.Forms
+{
+    using System;
+    using System.IO;
+    using BootstrapMvc.Core;
+
+    public class HiddenValue : Element, IControlContext
+    {
+        #region IControlContext
+
+        public string FieldName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public object FieldValue { get; set; }
+
+        public Type DataType { get; set; }
+
+        public bool IsRequired { get; set; }
+
+        public string[] Errors { get; set; }
+
+        public bool HasErrors { get; set; }
+
+        public bool HasWarning { get; set; }
+
+        #endregion
+
+        protected override void WriteSelf(TextWriter writer)
+        {
+            var input = Helper.CreateTagBuilder("input");
+            input.MergeAttribute("type", "hidden", true);
+
+            input.MergeAttribute("id", FieldName, true);
+            input.MergeAttribute("name", FieldName, true);
+            if (IsRequired)
+            {
+                input.MergeAttribute("required", "required", true);
+            }
+
+            input.MergeAttribute("value", FieldValue?.ToString(), true);
+
+            ApplyCss(input);
+            ApplyAttributes(input);
+
+            input.WriteFullTag(writer);
+        }
+    }
+}
---
+++ src/BootstrapMvc.BootstrapCommon/Components/Forms/HiddenValueExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace BootstrapMvc
+{
+    using System;
+    using System.Linq.Expressions;
+    using BootstrapMvc.Core;
+    using BootstrapMvc.Forms;
+
+    public static class HiddenValueExtensions
+    {
+        public static IItemWriter<HiddenValue> HiddenFor<TModel, TProperty>(this IAnyContentMarker<TModel> contentHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            var fg = contentHelper.CreateWriter<HiddenValue>();
+            contentHelper.Context.Helper.FillControlContext(fg.Item, expression);
+            return fg;
+        }
+    }
+}
